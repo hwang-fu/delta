2d73805c15afdefc146fdd75d76be2b36cb7d4f4
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 13:39:26 +0100

    feat: add Storage.is_empty()

--- src/tensor/storage.rs
+++ src/tensor/storage.rs
@@ -36,4 +36,9 @@ impl Storage {
     pub fn len(&self) -> usize {
         self.data.len()
     }
+
+    /// Returns true if storage is empty.
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
 }
